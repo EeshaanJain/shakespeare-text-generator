b99604778c6a1d9ebc363afbae2eecf425d75bc6
Eeshaan Jain <jaineeshaan17@gmail.com>
2020-12-28 13:23:10 +0530

    subtle changes

--- predict.py
+++ predict.py
@@ -1,15 +1,37 @@
-import numpy as np
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import tensorflow as tf
 
-path = tf.keras.utils.get_file('shakespeare.txt',
-                               'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
-text = open(path, 'rb').read().decode(encoding='utf-8')
-vocab = sorted(set(text))
-char2idx = {unique: i for i, unique in enumerate(vocab)}
-idx2char = np.array(vocab)
+
+# tf.get_logger().setLevel('ERROR')
+# physical_devices = tf.config.list_physical_devices('GPU')
+# tf.config.experimental.set_memory_growth(physical_devices[0], True)
+# # path = tf.keras.utils.get_file('shakespeare.txt',
+#                                'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+# text = open(path, 'rb').read().decode(encoding='utf-8')
+# vocab = sorted(set(text))
+# char2idx = {unique: i for i, unique in enumerate(vocab)}
+# idx2char = np.array(vocab)
+model = tf.keras.models.load_model('model1.h5')
+char2idx = {'\n': 0, ' ': 1, '!': 2, '$': 3, '&': 4, "'": 5, ',': 6, '-': 7,
+            '.': 8, '3': 9, ':': 10, ';': 11, '?': 12, 'A': 13, 'B': 14,
+            'C': 15, 'D': 16, 'E': 17, 'F': 18, 'G': 19, 'H': 20, 'I': 21,
+            'J': 22, 'K': 23, 'L': 24, 'M': 25, 'N': 26, 'O': 27, 'P': 28,
+            'Q': 29, 'R': 30, 'S': 31, 'T': 32, 'U': 33, 'V': 34, 'W': 35,
+            'X': 36, 'Y': 37, 'Z': 38, 'a': 39, 'b': 40, 'c': 41, 'd': 42,
+            'e': 43, 'f': 44, 'g': 45, 'h': 46, 'i': 47, 'j': 48, 'k': 49,
+            'l': 50, 'm': 51, 'n': 52, 'o': 53, 'p': 54, 'q': 55, 'r': 56,
+            's': 57, 't': 58, 'u': 59, 'v': 60, 'w': 61, 'x': 62, 'y': 63,
+            'z': 64}
+idx2char = ['\n', ' ', '!', '$', '&', "'", ',', '-', '.', '3', ':', ';', '?',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b',
+            'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+            'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-def generate_text(model, start_string):
+
+def generate_text(start_string):
     num_generate = 1000
     input_eval = [char2idx[s] for s in start_string]
     input_eval = tf.expand_dims(input_eval, 0)
